7ece8494698903710cd0d5c43ca967e67734e5e7
Kateřina Zagorová <kate.zagorova@gmail.comTUD500505:piskvorky kzagorova$ git config --global user.name Katerina ZagorovaTUD500505:piskvorky kzagorova$ git add .$ git add .$ git commit -m Doplnn>
2021-04-16 15:17:42 +0200

    4.úkol pokračování

--- index.js
+++ index.js
@@ -3,3 +3,24 @@
 let player = 'circle';
 
 const poleElement = document.querySelector('.hra-circle');
+
+
+const hra = (hraje) => {
+    if (player === 'circle') {
+      hraje.target.classList.add('ctverec--circle');
+      poleElement.src = 'images/cross.svg';
+      player = 'cross';
+   
+    } else if (player === 'cross') {
+      hraje.target.classList.add('ctverec--cross');
+      poleElement.src = 'images/circle.svg';
+      player = 'circle';
+    }
+  };
+  
+  const pole = document.querySelectorAll('.ctverec');
+  for (let i = 0; i < pole.length; i++) {
+
+    pole[i].addEventListener('click', hra);
+  }
+  
